2d1f7b1dc417d286ce0a6e619670918bddf7f138
Ulion Pardede <ulionpardede28@gmail.com>
2025-05-23 00:39:19 +0700

    Update Progress

--- pages/code.vue
+++ pages/code.vue
@@ -165,7 +165,7 @@
             <button
               type="button"
               @click="closeModal"
-              class="mr-4 text-gray-500"
+              class="mr-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
             >
               Cancel
             </button>
