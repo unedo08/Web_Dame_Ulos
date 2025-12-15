2ec227428fbf0bcdcbd4fe5336902adb6bb5160d
unedo08 <manaluunedo80@gmail.com>
2025-12-15 21:57:10 +0700

    update route export data packagng

--- routes/api/pengirimanBarangT.php
+++ routes/api/pengirimanBarangT.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PengirimanBarangTController;
 
-
+Route::get('/export-data', [PengirimanBarangTController::class, 'exportPengirimanBarang']);
 Route::get('/get-transaksi-detail/{id}', [PengirimanBarangTController::class, 'getPengirimanBarangByTransaksiId']);
 Route::get('/get-pengiriman', [PengirimanBarangTController::class, 'getPengiriman']);
 Route::get('/', [PengirimanBarangTController::class, 'index']); 
