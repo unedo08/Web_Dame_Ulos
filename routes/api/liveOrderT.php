3e88ff21d48158392e9a965916973b852c591f2b
“unedo-swing” <unedo@getswing.app>
2026-01-30 11:45:00 +0700

    fix api get data live per user

--- routes/api/liveOrderT.php
+++ routes/api/liveOrderT.php
@@ -9,10 +9,18 @@ Route::get('/show-live/{id}', [LiveOrderTController::class, 'show']);    // Get 
 Route::post('/store-live', [LiveOrderTController::class, 'store']);       // Create
 Route::put('/update-live/{id}', [LiveOrderTController::class, 'update']);  // Update
 Route::delete('/delete-live/{id}', [LiveOrderTController::class, 'destroy']); // Delete
-Route::get('/data-live/{namaAkun}', [LiveOrderTController::class, 'getLiveOrderByNamaAkun']);        // Get all
-Route::get('/data-live/{id}', [LiveOrderTController::class, 'getLiveOrderByLiveId']);  
+// Route::get('/data-live/{id}', [LiveOrderTController::class, 'getLiveOrderByLiveId'])
+//     ->whereNumber('id');
+// Route::get('/data-live/{namaAkun}', [LiveOrderTController::class, 'getLiveOrderByNamaAkun'])->where('namaAkun', '.*');
+
 
 // Custom route to get count by nama akun
 Route::get('/getAmountLive', [LiveOrderTController::class, 'countByNamaAkun']);
-Route::get('/data-table', [LiveOrderTController::class, 'getDataTabelLiveOrder']); 
-Route::patch('/{id}/check', [LiveOrderTController::class, 'updateStatusCheck']); 
+Route::get('/data-table', [LiveOrderTController::class, 'getDataTabelLiveOrder']);
+Route::patch('/{id}/check', [LiveOrderTController::class, 'updateStatusCheck']);
+
+Route::get('/data-live/{id}', [LiveOrderTController::class, 'getLiveOrderByLiveId'])
+    ->whereNumber('id');
+
+Route::get('/data-live/{namaAkun}', [LiveOrderTController::class, 'getLiveOrderByNamaAkun'])
+    ->where('namaAkun', '.*');
