a96e43505b6baf9289d547443ab7d2ffb7f9aba0
Ulion Pardede <ulionpardede28@gmail.com>
2026-07-02 09:36:13 +0700

    Update role akses qc untuk menu barang keluar

--- components/Sidebar/Menu.vue
+++ components/Sidebar/Menu.vue
@@ -103,7 +103,7 @@ const roleAccess = {
   "super-admin": "all",
   admin: ["Akun Pembeli", "Beranda", "Code", "Barang Masuk", "Live", "Kasir", "Inventory", "Acara", "Database Penjualan", "Cek Produk","Database Inventory", "Keuangan", "Barang Keluar"],
   marketing: ["Akun Pembeli", "Beranda", "Barang Masuk", "Live", "Kasir", "Packaging", "Acara", "Database Penjualan", "Database Inventory", "Cek Produk"],
-  "quality-control": ["Code", "Barang Masuk", "Kasir", "Inventory", "Database Penjualan", "Database Inventory"],
+  "quality-control": ["Code", "Barang Masuk", "Kasir", "Inventory", "Database Penjualan", "Database Inventory", "Barang Keluar"],
   packaging: ["Akun Pembeli", "Live", "Kasir", "Packaging", "Inventory", "Database Penjualan", "Database Inventory"],
   "pewarna-alam": ["Pewarna Alam", "Beranda"],
   "sosial-media": ["Kasir", "Acara"],
